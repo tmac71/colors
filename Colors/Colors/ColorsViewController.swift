7e2b49a96758ee5e8ebd4278415a05e29d59c0e9
Todd McIntosh <mcintoshtw@gmail.com>
2020-08-29 11:44:16 -0500

    first colors commit

---
+++ Colors/Colors/ColorsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ColorsViewController.swift
+//  Colors
+//
+//  Created by Todd McIntosh on 8/29/20.
+//  Copyright © 2020 Todd McIntosh. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath ) -> UITableViewCell {
+        //indexPath.section
+        //indexPath.row
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        cell.textLabel?.text = colors[indexPath.row]
+        
+        return cell
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
